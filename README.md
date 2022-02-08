5ab5d170dc71fd5f5e393ed4f8227be7c06fd13a
Ernest Nguyen <ernest.nguyen@gmail.com>
2022-02-08 00:36:07 -0500

    Added initial implementation of file donwloading using curl

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 
 This Python program is supposed to be able to download multiple files using the shell *curl* command.
 
-Uses Python 3.7.3
+Developed on Python 3.7.3. Not sure about compatibility. But subprocess.run() needs Python 3.5 at least
 
 Will decide later on the program flow and features.
 
-### Some dependencies
+## Use
+1. Open shell in macOS or Linux
+2. *python3 file_dwld.py* or *python file_dwld.py*
+3. No user input for now
+
+## Some dependencies
+- macOS and Linux compatibility
 - subprocess module
--- file_dwld.py
+++ file_dwld.py
@@ -1,2 +1,21 @@
 import subprocess
 
+# TODO Maybe be able to choose the destination folder (like Downloads for example) 
+
+sut_url = 'https://static.wikia.nocookie.net/iz-one/images/4/4c/One-reeler_Digital.jpg'
+sut_filename = 'izone-pic.jpg'
+
+default_dir = 'output'
+curl_flag = '-o'
+
+args = ['curl', curl_flag, sut_filename, sut_url]
+
+# Create the output directory if it does not exist already
+# This may change
+subprocess.run(['mkdir', default_dir])
+
+
+# Execute the curl command
+subprocess.Popen(args, cwd=default_dir).wait()
+
+print("Done")
